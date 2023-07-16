a769ffb40186abc391bb748eb2344aff08691419
Juan Morais <timrodz@icloud.com>
2023-07-16 13:07:04 -0500

    chore: Cloudflare support

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,16 @@
 
 declare global {
 	namespace App {
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
+
 		// interface Locals {}
 		// interface PageData {}
 		// interface Error {}

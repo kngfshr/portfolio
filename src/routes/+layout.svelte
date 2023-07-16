736532357179d13c557f6cdf870822b4e5a7f0f3
Juan Morais <timrodz@icloud.com>
2023-07-16 13:39:24 -0500

    fix: w-full instead of w-screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,12 @@
 	import '../app.css';
 </script>
 
-<div class="relative isolate gap-x-6 bg-gray-50 px-6 py-2.5 sm:px-3.5">
+<div class="relative isolate bg-gray-50 px-6 py-2.5 sm:px-3.5">
 	<p class="text-sm md:text-md leading-6 text-gray-900 text-center mx-auto">
 		This site is under construction. We strongly believe in shipping fast and iterating faster!
 	</p>
 </div>
 
-<div class="z-0 relative flex flex-col items-center py-24 px-4 md:px-24 w-screen overflow-x-hidden">
+<div class="z-0 relative flex flex-col items-center py-24 px-4 md:px-24 w-full overflow-x-hidden">
 	<slot />
 </div>
